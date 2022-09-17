5b89fe6637b129d063fd2007da45b323a73a0c78
TahaShek <sohailtaha128@gmail.com>
2022-09-18 00:29:40 +0500

    commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// let  CreateCard=()=>{
+//     let title =document.querySelector('.Name ').value;
+//     let list=document.querySelector('.list ').value;
+//     // let inputUrl= document.querySelector('.Url').value;
+
+//     // console.log(CardName,inputUrl);
+// // let CardObject=new Object();
+// // CardObject.Title=title;
+// // CardObject.ImageUrl=list;
+// // console.log(CardObject)
+// // CardObject.Id=Math.floor(Math.random()*1000);
+// // CardObject.Points=0;
+// // console.log(CardObject)
+// let CardContainer = document.querySelector('.CardContainer');
+// // let notes =document.createElement('div')
+// CardContainer.innerHTML += `
+// <div class="col-lg-3 mx-3 my-3">
+// <div class="card" style="width: 18rem;">
+
+// <div class="card-body ">
+//   <p>Name:${title}</p>
+//   <p>Points:${list}</p>
+// </div>
+//     </div>
+// </div>
+// `
+// // CardContainer.append(notes)
+// }
+
+
+let btn=document.querySelector('.btn')
+btn.addEventListener('click',()=>{
+  
+})
+
+
+
+
+
+
