d6f8ddb2f4f5edf6c44892e855db20e18a97fecc
TahaShek <sohailtaha128@gmail.com>
2022-09-18 20:30:58 +0500

    commit

--- index.js
+++ index.js
@@ -14,35 +14,48 @@
 // let CardContainer = document.querySelector('.CardContainer');
 // // let notes =document.createElement('div')
 // CardContainer.innerHTML += `
-// <div class="col-lg-3 mx-3 my-3">
-// <div class="card" style="width: 18rem;">
+{/* <div class="col-lg-3 mx-3 my-3">
+<div class="card" style="width: 18rem;">
 
-// <div class="card-body ">
-//   <p>Name:${title}</p>
-//   <p>Points:${list}</p>
-// </div>
-//     </div>
-// </div>
+<div class="card-body ">
+  <p>Name:${title}</p>
+  <p>Points:${list}</p>
+</div>
+    </div>
+</div> */}
 // `
 // // CardContainer.append(notes)
 // }
+
 
 let CreateNotes=()=>{
    let title=document.querySelector('.title').value 
    let work=document.querySelector('.Work').value
 let notes=document.querySelector('.Notes')
-notes.innerHTML += `
-<div class="col-md-4 col-sm-6 content-card">
-      <div class="card-big-shadow">
-          <div class="card card-just-text">
-              <div class="content">
-                  <h4 class="title">asdas</h4>
-                  <p class="description">sadas </p>
-              </div>
-          </div> <!-- end card -->
-      </div>
+let div =document.createElement('div')
+
+let r=Math.floor(Math.random()*256)
+let g=Math.floor(Math.random()*256)
+let b=Math.floor(Math.random()*256)
+// body.style.backgroundColor='red'
+// notes.style.backgroundColor=`rgb(${r},${g},${b})`
+div.style.backgroundColor=`rgb(${r},${g},${b})`
+div.classList.add('col-md-9')
+div.style.color='white'
+div.style.width='500px'
+div.innerHTML=`
+  <div class="card-body my-3">
+    <h5 class="card-title">Title:${title}</h5>
+    <p class="card-text">Your Work:${work}</p>
+    <button class="btn remove btn-outline-dark w-75 ">Remove</button>
   </div>
-`
+
+  `
+div.querySelector('.remove')
+div.addEventListener('click',()=>{
+  notes.remove()  
+})
+notes.append(div)
 }
 
 
