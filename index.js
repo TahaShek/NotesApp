da4cd3167ff415ee5c3b43ae54665d2b133e1533
TahaShek <sohailtaha128@gmail.com>
2022-09-19 15:06:38 +0500

    commit

--- index.js
+++ index.js
@@ -5,25 +5,28 @@ btn.addEventListener('click',()=>{
   let title= document.querySelector('.title').value 
 let work =document.querySelector('.work').value 
   let CreateNotes=document.querySelector('.NotesContainer');
+  if(title==='' || work===''){
+    alert('Enter Valid Requirements')
+  }
+  return
   // console.log(work)
 let div=document.createElement('div')
 div.classList.add('col-md-4')
 
-div.innerHTML= `
 
+div.innerHTML= `
 <div class="mb-3">
  <h3>${title}</h3>
- <textarea class="form-control " placeholder="Enter Your Work" name="" id="" rows="5">${work}</textarea>
+ <textarea class="form-control " placeholder="Enter Your Work" name="" id="" rows="7">${work}</textarea>
  <button type = "button" class = "btn del" id = "delete-all-btn">
  <span><i class = "fas fa-trash"></i></span>
  Delete All
 </button> 
 </div>
-
 `
 div.querySelector('.del')
 div.addEventListener('click',()=>{
-  div.remove()
+ div.remove()
 })
 
 CreateNotes.append(div)
